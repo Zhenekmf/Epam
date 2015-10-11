a62e7d62562ddbe10419e4d229f1857638d5c46c
Zhenekmf <zhenek94@mail.ru>
2015-10-11 03:03:11 +0300

    update 1,with javadoc

--- 6/src/Notebook.java
+++ 6/src/Notebook.java
@@ -1,14 +1,25 @@
 import java.util.ArrayList;
-
+/**
+ * 
+ * @author Zhenek
+ *
+ */
 public class Notebook {
 	private ArrayList<Record> list = new ArrayList<Record>();
 	private int numberOfRecords;
-
+	/**
+	 * Adds new record
+	 * @param msg String to add in new record
+	 */
 	public void addRecord(String msg) {
 		list.add(new Record(msg));
 		numberOfRecords++;
 	}
-
+/**
+ * Deletes record from array
+ * @param id ID of record in array, counted from zero 
+ * @throws Exception in case of incorrect id (outOfBounds)
+ */
 	public void delRecord(int id) throws Exception {
 		try {
 			list.remove(id);
@@ -20,7 +31,13 @@ public class Notebook {
 		}
 
 	}
-
+	/**
+	 * Edits record by replacing it with new one or appending a string to it
+	 * @param id ID of record to edit
+	 * @param msg Message to append\replace with
+	 * @param append Mode changer; Use "true" to append, "false" to replace
+	 * @throws Exception outOfBounds in case of incorrect ID
+	 */
 	public void editRecord(int id, String msg, Boolean append) throws Exception {
 		try {
 			if (append) {
@@ -34,7 +51,12 @@ public class Notebook {
 
 		}
 	}
-
+	/**
+	 * Shows the record 
+	 * @param id ID of record to show
+	 * @return Record text
+	 * @throws Exception outOfBounds in case of incorrect ID
+	 */
 	public String showRecord(int id) throws Exception {
 		try {
 			return Integer.toString(id) + ":" + list.get(id).getText();
@@ -43,7 +65,11 @@ public class Notebook {
 		}
 
 	}
-
+	/**
+	 * Shows all records
+	 * @return Number of records and strings with all records, divided with newlines
+	 * @throws Exception outOfBounds in case of incorrect ID, looks to be inherited from showRecord
+	 */
 	public String showAll() throws Exception {
 		String ret = "Total number of records:"+Integer.toString(numberOfRecords)+"\n";
 		for (int i = 0; i < list.size(); i++) {
--- 6/src/Record.java
+++ 6/src/Record.java
@@ -1,7 +1,16 @@
+/**
+ * 
+ * @author Zhenek
+ *
+ */
 public class Record {
+	
 	private String text;
 	
-
+	/**
+	 * Generates new record with given text
+	 * @param msg String to use
+	 */
 	public Record(String msg) {
 		this.text=msg;
 	}
